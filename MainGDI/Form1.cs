4a6b4e646ce0b99548796013c6566348ea59e5fa
gafar113 <falgamerss@gmail.com>
2021-02-09 12:19:12 +0300

    A basic GDIOverlay

---
+++ MainGDI/Form1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MainGDI
+{
+    public partial class Form1 : Form
+    {
+        FormOverlay frm = new FormOverlay();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void checkBox2_CheckedChanged(object sender, EventArgs e)
+        {
+            if (checkBox2.Checked == true)
+            {
+                frm.Show();
+            }
+            else
+            {
+                frm.Hide();
+            }
+        }
+    }
+}
---
+++ MainGDI/FormOverlay.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MainGDI
+{
+    public partial class FormOverlay : Form
+    {
+        private RECT rect;
+        public const string WINDOW_NAME = "3DXChat";
+        private IntPtr handle = FindWindow(null, WINDOW_NAME);
+
+        public struct RECT
+        {
+            public int left, top, right, bottom;
+        }
+
+        
+        [DllImport("user32.dll")]
+        static extern int SetWindowLong(IntPtr hWnd, int nIndex,int dwNewLong);
+        
+        [DllImport("user32.dll",SetLastError = true)]
+        static extern int GetWindowLong(IntPtr hWnd, int nIndex);
+
+        [DllImport("user32.dll", SetLastError = true)]
+        static extern IntPtr FindWindow(string lpClassName, string lpWindowName);
+
+        [DllImport("user32.dll")]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        public static extern bool GetWindowRect(IntPtr hwnd, out RECT lpRect);
+
+        public FormOverlay()
+        {
+            InitializeComponent();
+        }
+
+        private void FormOverlay_Load(object sender, EventArgs e)
+        {
+            this.BackColor = Color.BlanchedAlmond;
+            this.TransparencyKey = Color.BlanchedAlmond;
+            this.TopMost = true;
+           this.FormBorderStyle = FormBorderStyle.None;
+
+            int initialStyle = GetWindowLong(this.Handle, -20);
+            SetWindowLong(this.Handle, -20, initialStyle | 0x80000 | 0x20);
+            GetWindowRect(handle, out rect);
+            this.Size = new Size(rect.right - rect.left, rect.bottom - rect.top);
+            this.Top = rect.top;
+            this.Left = rect.left;
+        }
+
+        private Graphics g;
+        private Pen myPen = new Pen(Color.BlueViolet);
+
+        private void FormOverlay_Paint(object sender, PaintEventArgs e)
+        {
+            g = e.Graphics;
+            g.DrawRectangle(myPen,100,100,200,200);
+        }
+    }
+}
